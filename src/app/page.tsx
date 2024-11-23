fa4438ff157be91d62f4e1ae7f3c4d98ab91968a
ffjaervik <fabianfjaervik@gmail.com>
2024-11-23 18:20:36 +0100

    images in webp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import PolygonCarousel from '@/components/polygon-carousel'
 
 export default function Home() {
   return (
-    <div className=" p-8 pb-20 container sm:py-20">
-      <main className="">
+    <div className="p-8 pb-20 sm:py-20">
+      <main className="container sm:w-96">
         <PolygonCarousel></PolygonCarousel>
       </main>
     </div>
